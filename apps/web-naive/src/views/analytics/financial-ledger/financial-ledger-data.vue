193276015664715ee9a21923a2cbf27628fa7a8b
ju4tlearn <ju4tlearn@gmail.com>
2024-11-05 20:24:39 +0800

    :sparkles: 完善 财务台账 页面

--- apps/web-naive/src/views/analytics/financial-ledger/financial-ledger-data.vue
+++ apps/web-naive/src/views/analytics/financial-ledger/financial-ledger-data.vue
@@ -1,3 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, watch } from 'vue';
 
-<template>财务台账数据</template>
+import { useAnalyticsStore } from '../client';
+import CustomTable from '../components/custom-table.vue';
+import {
+  contractStartDateColumn,
+  financialLedgerDataRef,
+  generateFinancialLedgerDataList,
+  initColumns,
+  projectColumn,
+} from './client';
+
+const store = useAnalyticsStore();
+
+onMounted(() => {
+  generateFinancialLedgerDataList(50);
+  store.updateProjectItems(financialLedgerDataRef.value);
+});
+
+watch(
+  () => store.selectedProject,
+  (value) => {
+    projectColumn.filterOptionValue = value;
+  },
+);
+
+watch(
+  () => store.checkRange,
+  (value) => {
+    contractStartDateColumn.filterOptionValue = value;
+  },
+);
+</script>
+
+<template>
+  <CustomTable :columns="initColumns()" :data="financialLedgerDataRef" />
+</template>
